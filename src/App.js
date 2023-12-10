9a7e94d06fb77dfbc09b0d61540ae700b5cb1064
Пользователь <freeefireee1234f@gmail.com>
2023-12-10 09:24:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.js';
+import './style.css';
 
 function App() {
+  const [input1, setInput1] = useState('');
+const [input2, setInput2] = useState('');
+const [result, setResult] = useState('');
+
+const handleInputChange = (e, inputType) => {
+  const value = e.target.value;
+  if (inputType === 'input1') {
+    setInput1(value);
+  } else {
+    setInput2(value);
+  }
+};
+
+const handleOperation = (operation) => {
+  const num1 = parseFloat(input1);
+  const num2 = parseFloat(input2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    setResult('Invalid input');
+    return;
+  }
+
+  switch (operation) {
+    case '+':
+      setResult(num1 + num2);
+      break;
+    case '-':
+      setResult(num1 - num2);
+      break;
+    case '*':
+      setResult(num1 * num2);
+      break;
+    case '/':
+      if (num2 === 0) {
+        setResult('Division by zero');
+      } else {
+        setResult(num1 / num2);
+      }
+      break;
+    case 'sqrt':
+      setResult(Math.sqrt(num1));
+      break;
+    case 'pow':
+      setResult(Math.pow(num1, num2));
+      break;
+    default:
+      setResult('Invalid operation');
+      break;
+  }
+};
+
   return (
+    <div className='calc'>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='one-h1'>Simple Calculator</h1>
+      <div>
+        <input className='inp' type="text" value={input1} onChange={(e) => handleInputChange(e, 'input1')} />
+        <input className='inp' type="text" value={input2} onChange={(e) => handleInputChange(e, 'input2')} />
+      </div>
+      <div>
+        <button className='btn' id='btn1' onClick={() => handleOperation('+')}>+</button>
+        <button className='btn' id='btn2' onClick={() => handleOperation('-')}>-</button>
+        <button className='btn' id='btn3' onClick={() => handleOperation('*')}>*</button>
+        <button className='btn' id='btn4' onClick={() => handleOperation('/')}>/</button>
+        <button className='btn' id='btn5' onClick={() => handleOperation('sqrt')}>√</button>
+        <button className='btn' id='btn6' onClick={() => handleOperation('pow')}>^</button>
+      </div>
+      <div>
+        <p className='result'>Result: {result}</p>
+      </div>
+    </div>
     </div>
   );
 }
+  
 
 export default App;
